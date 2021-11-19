95d0c45aeb52d3bc1c3304e820663b5837607a68
gitneh <kc-nehuen@hotmail.com>
2021-11-19 14:26:16 -0300

    cambios

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -7,13 +7,13 @@ function sumar(e) {
     let campo2 = parseFloat (document.getElementById('campo2').value);
     let campo3 = parseFloat (document.getElementById('campo3').value);
 //  Evitar que los campos queden en blanco   
-    if (isNaN((campo1 !="") && (campo2 !="") && (campo3 !=""))) {
+    if (isNaN(sumaT)) {
         $("#tbody").append(`<div>
         <h3>Debe ingresar el valor anual del contrato en los campos especificados</h3>
         </div>`);
     }else{
         let sumaT = (campo1+campo2+campo3);
-        let comision = (sumaT * 0.4).toFixed;
+        let comision = Math.round (sumaT * 0.4);
 //  Colocar el resultado en el HTML
     $("#tbody").append(`<div>
                         <h3>El valor de la comisión es de $${comision}</h3>
