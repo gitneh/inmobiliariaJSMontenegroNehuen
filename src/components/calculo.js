b6dbb06787b09585ff14e08598b3168be05f8ceb
gitneh <kc-nehuen@hotmail.com>
2021-11-19 13:56:05 -0300

    cambios

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -3,13 +3,13 @@ miFormulario.addEventListener('submit',sumar);
 //  Función para capturar valores del campo, y calcularlos
 function sumar(e) {
     e.preventDefault();
-    let campo1 = document.getElementById('campo1').value;
-    let campo2 = document.getElementById('campo2').value;
-    let campo3 = document.getElementById('campo3').value;
+    let campo1 = parseFloat (document.getElementById('campo1').value);
+    let campo2 = parseFloat (document.getElementById('campo2').value);
+    let campo3 = parseFloat (document.getElementById('campo3').value);
 //  Evitar que los campos queden en blanco   
     if ((campo1 !="") && (campo2 !="") && (campo3 !="")) {
-        let sumaT = parseFloat (campo1+campo2+campo3);
-        let comision = parseFloat (sumaT * 0.4);
+        let sumaT = (campo1+campo2+campo3);
+        let comision = (sumaT * 0.4);
 //  Colocar el resultado en el HTML
     $("#tbody").append(`<div>
                         <h3>El valor de la comisión es de $${comision}</h3>
