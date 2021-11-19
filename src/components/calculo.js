5e73a09f5310d25e6e3b0a9135e40706c0915ce5
gitneh <kc-nehuen@hotmail.com>
2021-11-19 10:36:33 -0300

    cambios

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -8,42 +8,8 @@ function sumar(e) {
     let campo3 = parseFloat (document.getElementById('campo3').value);
 
     let sumaT = campo1+campo2+campo3
-    console.log(sumaT)
+    $("#tbody").append(`<div>
+                        <h3>La suma total del contrato es de $${sumaT}</h3>`
+
+    )
 }
-//     if(localStorage.getItem('suma') === null){
-
-//         let suma = [];
-//         suma.push(sumaT)
-//         localStorage.setItem('suma', JSON.stringify(suma))
-//     }else {
-//         let suma = JSON.parse(localStorage.getItem('suma'))
-//         suma.push(sumaT)
-//         localStorage.setItem('suma', JSON.stringify(suma))
-//     }
-//     leer();
-//     document.getElementById('form').reset();
-
-
-// function leer(){
-
-//     let suma = JSON.parse(localStorage.getItem('suma'));
-
-//     document.getElementById('tbody').innerHTML = "";
-
-//     for (let i = 0; i < suma.length; i++) {
-        
-//         let campo1 = suma[i].document.getElementById('campo1').value;
-//         let campo2 = suma[i].document.getElementById('campo2').value;
-//         let campo3 = suma[i].document.getElementById('campo3').value;
-//         let sumaT = campo1+campo2+campo3;
-//         document.getElementById('tbody').innerHTML += 
-//         ` 
-//         <div class="input-group mb-3">
-//         <span class="input-group-text">$</span>
-//         <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-//         <span class="input-group-text">${sumaT}</span>
-//       </div>
-//         `
-//     }
-// }
-// }
