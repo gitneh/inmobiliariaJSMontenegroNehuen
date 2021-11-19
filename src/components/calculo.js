ca1fc42aeab94cdafcaebe584cbeb87d97164138
gitneh <kc-nehuen@hotmail.com>
2021-11-19 13:23:51 -0300

    cambios

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -6,12 +6,17 @@ function sumar(e) {
     let campo1 = parseFloat (document.getElementById('campo1').value);
     let campo2 = parseFloat (document.getElementById('campo2').value);
     let campo3 = parseFloat (document.getElementById('campo3').value);
-
-    let sumaT = parseFloat (campo1+campo2+campo3);
-    let comision = parseFloat (sumaT * 0.4);
-    
-$("#tbody").append(`<div>
-                    <h3>El valor de la comisión es de $${comision}</h3>
-                    </div>`);
+    if ((campo1 !='') && (campo2 !='') && (campo3 !='')){
+        let sumaT = parseFloat (campo1+campo2+campo3);
+        let comision = parseFloat (sumaT * 0.4);
+        
+    $("#tbody").append(`<div>
+                        <h3>El valor de la comisión es de $${comision}</h3>
+                        </div>`);
+    }else{
+        $("#tbody").append(`<div>
+        <h3>Debe ingresar el valor anual del contrato en los campos especificados</h3>
+        </div>`);
+    }
 }
 
