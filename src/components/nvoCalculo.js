88640e37cb534bfb3535ae09516d88c34206e9d2
gitneh <kc-nehuen@hotmail.com>
2021-11-19 07:13:05 -0300

    cambios

--- src/components/nvoCalculo.js
+++ src/components/nvoCalculo.js
@@ -1,25 +1,33 @@
 const contenedorContratos = document.getElementById ('contratosJSON');
 
-let contratos = [];
-$.getJSON('datos.json', function (data) {
+let contratos =[];
+
+$.getJSON('data/datos.json', function (data) {
+    console.log(data)
     mostrarContratos (data)
 })
 
 mostrarContratos(contratos)
 function mostrarContratos (array){
     for (const contrato of array) {
-        let div = document.createElement('div');
-        div.innerHTML += (`<div>
-                            <h4> ID: ${contrato.idContrato} </h4>
-                            <p> Nombre: ${contrato.nombre}</p>
-                            <b> $ ${contrato.valorContrato}</b>
-                            </div>`);
-        contenedorContratos.appendChild(div)
+                 $("#contratosJSON").prepend(`<div>
+                                         <h4> ID: ${contrato.idContrato} </h4>
+                                         <p> Nombre: ${contrato.nombre}</p>
+                                         <b> $ ${contrato.valorContrato}</b>
+                                         </div>`);
+        // let div = document.createElement('div');
+        // div.innerHTML += (`<div>
+        //                     <h4> ID: ${contrato.idContrato} </h4>
+        //                     <p> Nombre: ${contrato.nombre}</p>
+        //                     <b> $ ${contrato.valorContrato}</b>
+        //                     </div>`);
+        // contenedorContratos.appendChild(div)
     }
 }
+//             for (const dato of misDatos) {
+//                 $("#contratosJSON").prepend(`<div>
+//                                         <h4> ID: ${dato.idContrato} </h4>
+//                                         <p> Nombre: ${dato.nombre}</p>
+//                                         <b> $ ${dato.valorContrato}</b>
+//                                         </div>`);
 
-
-
-function agregarContrato () {
-
-}
