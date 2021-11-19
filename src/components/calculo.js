9bd5045ef63c7643bcbbbb0a29df4ef8820f698f
gitneh <kc-nehuen@hotmail.com>
2021-11-19 10:07:18 -0300

    cambios

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -1,48 +1,49 @@
+let miFormulario = document.getElementById('form');
+miFormulario.addEventListener('submit',sumar);
 
-document.getElementById('form').addEventListener('submit',crear);
-function crear(e) {
+function sumar(e) {
     e.preventDefault();
     let campo1 = document.getElementById('campo1').value;
     let campo2 = document.getElementById('campo2').value;
     let campo3 = document.getElementById('campo3').value;
 
     let sumaT = campo1+campo2+campo3
-    
+    console.log(sumaT)
+}
+//     if(localStorage.getItem('suma') === null){
 
-    if(localStorage.getItem('suma') === null){
-
-        let suma = [];
-        suma.push(sumaT)
-        localStorage.setItem('suma', JSON.stringify(suma))
-    }else {
-        let suma = JSON.parse(localStorage.getItem('suma'))
-        suma.push(sumaT)
-        localStorage.setItem('suma', JSON.stringify(suma))
-    }
-    leer();
-    document.getElementById('form').reset();
+//         let suma = [];
+//         suma.push(sumaT)
+//         localStorage.setItem('suma', JSON.stringify(suma))
+//     }else {
+//         let suma = JSON.parse(localStorage.getItem('suma'))
+//         suma.push(sumaT)
+//         localStorage.setItem('suma', JSON.stringify(suma))
+//     }
+//     leer();
+//     document.getElementById('form').reset();
 
 
-function leer(){
+// function leer(){
 
-    let suma = JSON.parse(localStorage.getItem('suma'));
+//     let suma = JSON.parse(localStorage.getItem('suma'));
 
-    document.getElementById('tbody').innerHTML = "";
+//     document.getElementById('tbody').innerHTML = "";
 
-    for (let i = 0; i < suma.length; i++) {
+//     for (let i = 0; i < suma.length; i++) {
         
-        let campo1 = suma[i].document.getElementById('campo1').value;
-        let campo2 = suma[i].document.getElementById('campo2').value;
-        let campo3 = suma[i].document.getElementById('campo3').value;
-        let sumaT = campo1+campo2+campo3;
-        document.getElementById('tbody').innerHTML += 
-        ` 
-        <div class="input-group mb-3">
-        <span class="input-group-text">$</span>
-        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-        <span class="input-group-text">${sumaT}</span>
-      </div>
-        `
-    }
-}
-}
+//         let campo1 = suma[i].document.getElementById('campo1').value;
+//         let campo2 = suma[i].document.getElementById('campo2').value;
+//         let campo3 = suma[i].document.getElementById('campo3').value;
+//         let sumaT = campo1+campo2+campo3;
+//         document.getElementById('tbody').innerHTML += 
+//         ` 
+//         <div class="input-group mb-3">
+//         <span class="input-group-text">$</span>
+//         <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+//         <span class="input-group-text">${sumaT}</span>
+//       </div>
+//         `
+//     }
+// }
+// }
