b1113eea57845261f30eb6cee708d217eea081ad
gitneh <kc-nehuen@hotmail.com>
2021-11-19 11:11:08 -0300

    cambios

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -8,9 +8,7 @@ function sumar(e) {
     let campo3 = parseFloat (document.getElementById('campo3').value);
 
     let sumaT = campo1+campo2+campo3;
-    $("#tbody").append(`<div>
+}
+$("#tbody").append(`<div>
 <h3>La suma total del contrato es de $${sumaT}</h3>
 </div>`);
- 
-}
-
